e002988d71644be363710e775a41a96df27fe0eb
Loredana <loredana_bratu@yahoo.com>
2019-07-11 16:46:02 +0300

    routes

--- watchnext/src/App.js
+++ watchnext/src/App.js
@@ -1,14 +1,30 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
+import Login from './components/login/login'
+import Register from './components/register/register'
 
 
-function App() {
-  return (
-    <div className="App">
-     <h1> Heiaaa</h1>
+class App extends Component {
+    render() {
+      return (
+          <Router >
+  
+              <div className="conatainer">
+                <Switch>
+                  <Route exact path="/" component={Login} />
+                  <Route exact path="/" component={Register} />
+                </Switch>
+              </div>
+           
+          </Router>
      
-    </div>
-  );
+      );
+    }
 }
+  export default App;
+     
+     
+   
 
-export default App;
