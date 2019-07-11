3ec618eba9cde8934f88a43679bced95f0eb132e
ioanmanolea <ioanmanolea@gmail.com>
2019-07-11 17:42:12 +0300

    schita

---
+++ watchnext/src/Home/index.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import '../Home/index.css';
+
+
+class Home extends React.Component {
+    constructor() {
+        super()
+    }
+    
+    render() {
+        return (
+          <div><b>Home Page - Container</b></div>
+        )    
+    }
+}
+
+export default Home
