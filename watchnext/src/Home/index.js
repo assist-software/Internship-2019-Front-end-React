f9312032857d83b3e3f3433aeadff55c9bd04854
luizaalexandra <luizalautaru@yahoo.com>
2019-07-12 11:57:47 +0300

    home1

--- watchnext/src/Home/index.js
+++ watchnext/src/Home/index.js
@@ -9,9 +9,20 @@ class Home extends React.Component {
     
     render() {
         return (
-          <div><b>Home Page - Container</b></div>
-        )    
+        <div>
+            <div id="moon">
+                <div id="left">
+                    <h1 className="moon">Moonlight {this.props.name}</h1>
+                    <p className="p">A chronicle of the childhood, adolescence and burgeoning adulthood of a young, African-American, gay man growing up in a rough neighborhood of Miami.</p>
+                </div>
+                <div>
+                     <img className="img2" src={require('./image4.2.png')}/>
+                </div>
+            </div>
+        </div>
+             
+        );  
     }
-}
 
+}
 export default Home
