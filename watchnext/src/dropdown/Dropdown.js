bfbfed0832b86991f133111a3bd1d8c7b4cc8ef2
ioanmanolea <ioanmanolea@gmail.com>
2019-07-22 10:08:39 +0300

    up

--- watchnext/src/dropdown/Dropdown.js
+++ watchnext/src/dropdown/Dropdown.js
@@ -7,55 +7,74 @@ import { Scrollbars } from 'react-custom-scrollbars';
 
 class Dropdown extends React.Component {
 
-    constructor(props){
-        super(props)
-        this.state = {
-          listOpen: false,
-          headerTitle: props.gen,
-          list:this.props.list.map(item => <li onClick={()=>this.genderSelect(item.title)} key={item.id}>{item.title}</li>)
-        }
-        this.handleClick = this.handleClick.bind(this)
+  constructor(props) {
+    super(props)
+    this.state = {
+      listOpen: false,
+      headerTitle: this.props.gen,
+      catCount: {},
     }
+    this.handleClick = this.handleClick.bind(this)
+  }
 
+  handleClick() {
+    this.setState((prevState) => {
+      return {
+        listOpen: !prevState.listOpen
+      }
+    })
+  }
 
-    componentWillReceiveProps(){
-        this.setState({list:this.props.list.map(item => <li onClick={()=>this.genderSelect(item.title)} key={item.id}>{item.title}</li>)})
-    }
+  genreSelect(genre) {
+    this.setState(() => {
+      return {
+        listOpen: false,
+        headerTitle: genre.title
+      }
+    })
+    this.props.ug(genre.id)
+ 
+  }
 
-    handleClick(){
-       this.setState((prevState)=>{
-           return {
-                listOpen:!prevState.listOpen
-           }
-       })
-    }
-
-    genderSelect(title){
-        this.setState(()=>{
-            return{
-                listOpen: false,
-                headerTitle:title
-            }
+  getMovies = () => {
+    let url = "http://localhost:3001/category"
+    fetch(url)
+      .then(resp => resp.json())
+      .then(data => {
+        data.map(item => {
+          fetch("http://localhost:3001/movies?category_like=" + item.id)
+            .then(resp => resp.json())
+            .then(data => {
+              let movies = data.map(item => item)
+              this.setState((prev) => {catCount: prev.catCount[item.id] = movies.length }
+            )
         })
-        this.props.ug(title)
-    }
+      })
+    })
+  }
 
-    render() {
-        return (
-          <div id="dropDown">
-            <div name="genre" id="genre">
-                <button onClick={this.handleClick}>{this.state.headerTitle}<FontAwesomeIcon icon={this.state.listOpen?faChevronDown:faChevronLeft} /></button>
-            </div>
-            {this.state.listOpen && <div id="list"> 
-                <ul>
-                <Scrollbars style={{ height: 175 }}>
-                    {this.state.list}
-                    </Scrollbars>
-                </ul>
-            </div>}
-          </div>
-        )    
-    }
+  componentDidMount = () => {
+    this.props.ug(this.state.headerTitle)
+    if(this.props.num)
+      this.getMovies()
+  }
+
+  render() {
+    return (
+      <div id="dropDown">
+        <div name="genre" id="genre">
+          <button onClick={this.handleClick}>{this.state.headerTitle}<FontAwesomeIcon icon={this.state.listOpen ? faChevronDown : faChevronLeft} /></button>
+        </div>
+        {this.state.listOpen && <div id="list">
+          <ul>
+            <Scrollbars style={{ height: 175 }}>
+              {this.props.list.map(item => <li onClick={() => this.genreSelect(item)} key={item.id}>{item.title} {this.props.num && "(" + this.state.catCount[item.id]+ ")"}</li>)}
+            </Scrollbars>
+          </ul>
+        </div>}
+      </div>
+    )
+  }
 }
 
 export default Dropdown
