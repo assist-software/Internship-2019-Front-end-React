0735a45437d4fac3b4b341826fdf137d1d4f581d
petros0803 <b.petrosceac@gmail.com>
2019-07-23 17:37:27 +0300

    login/reset/reset functional

--- watchnext/src/api-connection.js
+++ watchnext/src/api-connection.js
@@ -2,15 +2,19 @@
 //     categories: 'http://localhost:3001/category/',
 //     movies: 'http://localhost:3001/movies/',
 //     catMovies: 'http://localhost:3001/movies?category_like=',
-//     singUp: 'http://localhost:3001/users'
+//     singUp: 'http://localhost:3001/users',
+//     logIn: 'http://localhost:3001/users',
+//     resetPass: 'http://localhost:3001/users'
 // }
 
 const api = {
-    
-    categories: 'https://cors-anywhere.herokuapp.com/http://localhost:3001/category/',
-    movies: 'https://cors-anywhere.herokuapp.com/http://localhost:3001/movies/',
-    catMovies: 'https://cors-anywhere.herokuapp.com/http://192.168.151.105/movies/?category_like=',
-    singUp: 'https://cors-anywhere.herokuapp.com/http://192.168.151.105:1234/signup/'
+    //https://cors-anywhere.herokuapp.com/
+    categories: 'http://localhost:3001/category/',
+    movies: 'http://localhost:3001/movies/',
+    catMovies: 'http://192.168.151.105/movies/?category_like=',
+    singUp: 'http://192.168.151.105:1234/signup/',
+    logIn: 'http://192.168.151.105:1234/login/',
+    resetPass: 'http://192.168.151.105:1234/reset/'
 }
 
 export default api
