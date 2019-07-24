e05283423d2b473d30589b8effaddd55d5a1c1ce
ioanmanolea <ioanmanolea@gmail.com>
2019-07-24 09:48:48 +0300

    update

--- watchnext/src/api-connection.js
+++ watchnext/src/api-connection.js
@@ -6,20 +6,22 @@ const api = {
     catMovies: base + '/movies?category_like=',
     singUp: base + '/users',
     addMovie: base + '/movies',
-    delMovie: base + '/movies/'
+    delMovie: base + '/movies/',
+    lastMovie: base + '/movies/2'
 }
 
 // const base = 'http://192.168.151.105:1234'
 // const api = { 
-//     categories: '/category/',
-//     movies: '/movies/',
-//     catMovies: '/movies/?category_like=',
-//     singUp: '/signup/',
-//     addMovie: '/movies/add',
-//     delMovie: '/movies/',
-//     singUp: '/signup/',
-//     logIn: '/login/',
-//     resetPass: '/reset/'
+//     categories: base + '/category/',
+//     movies: base + '/movies/',
+//     catMovies: base + '/movies/?category_like=',
+//     singUp: base + '/signup/',
+//     addMovie: base + '/movies/add',
+//     delMovie: base + '/movies/',
+//     lastMovie: base + '/movies/2',
+//     singUp: base + '/signup/',
+//     logIn: base + '/login/',
+//     resetPass: base + '/reset/'
 // }
 
 export default api
